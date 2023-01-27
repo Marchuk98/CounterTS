66a0bc9c2bf44567daf54fba0c219fe4b043f702
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-01-28 02:58:33 +0300

    Typing overturning props and rendering components

---
+++ src/components/SuperButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import s from './SuperButton.module.css'
+
+type SuperButtonPropsType = {
+    name:string
+    callBack:() => void
+    disabled: boolean
+}
+
+
+const SuperButton = (props: SuperButtonPropsType) => {
+
+    const onClickInButtonHandler = () => {
+        props.callBack();
+    }
+
+    return (
+        <div>
+            <button className={s.ButtonInc} disabled={props.disabled} onClick={onClickInButtonHandler}>{props.name}</button>
+        </div>
+    );
+};
+
+export default SuperButton;
