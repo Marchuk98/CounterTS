6ec2df3e076003d6d791e8c740b875178182aaf8
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-01-28 02:59:37 +0300

    Logic implementation and component rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Display from "./components/Display";
+import SuperButton from "./components/SuperButton";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  let [increment, setIncrement] = useState(0);
+
+  let maxLimit = 5;
+  const incrementCallback = () => {
+    increment < maxLimit && setIncrement(increment +1);
+  }
+
+  const resetResultCallback = () => {
+    setIncrement(0);
+  }
+
+  const incDisabled = increment === 5;
+  const resetDisabled = increment === 0;
+
+  return(
+      <div className='App'>
+        <Display count={increment} limit={maxLimit}/>
+        <div className={'AppButtonBlock'} >
+          <SuperButton name={'incr'} callBack={incrementCallback} disabled={incDisabled} />
+          <SuperButton name={'reset'} callBack={resetResultCallback} disabled={resetDisabled}/>
+        </div>
+      </div>
   );
 }
 
