5bf314fc12747434eebe3e5b2fa0221f397ed56a
Vladimir Marchuk <Gaba00275@yandex.ru>
2023-02-15 22:45:25 +0300

    Rendering components of settings and prescribing logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,77 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import Display from "./components/Display";
-import SuperButton from "./components/SuperButton";
+import Display from "./components/Counter/Display";
+import Settings from "./components/Settings/Settings";
 
 function App() {
 
-  let [increment, setIncrement] = useState(0);
 
-  let maxLimit = 5;
-  const incrementCallback = () => {
-    increment < maxLimit && setIncrement(increment +1);
-  }
+    let [valueSettings, setValueSettings] = useState({
+        startValue: 0,
+        maxValue: 0,
+    })
+    const [increment, setIncrement] = useState<number>(0);
+    const [error, setError] = useState<boolean>(false)
 
-  const resetResultCallback = () => {
-    setIncrement(0);
-  }
 
-  const incDisabled = increment === 5;
-  const resetDisabled = increment === 0;
+    useEffect(() => {
+        let newStartValueStr = localStorage.getItem("startValue")
+        let newMaxValueStr = localStorage.getItem("maxValue")
+        if (newStartValueStr && newMaxValueStr) {
+            let newStartValue = JSON.parse(newStartValueStr)
+            let newMaxValue = JSON.parse(newMaxValueStr)
+            setValueSettings({...valueSettings, startValue: newStartValue, maxValue: newMaxValue})
+            setIncrement(valueSettings.startValue)
+        }
+    }, [])
 
-  return(
-      <div className='App'>
-        <Display count={increment} limit={maxLimit}/>
-        <div className={'AppButtonBlock'} >
-          <SuperButton name={'incr'} callBack={incrementCallback} disabled={incDisabled} />
-          <SuperButton name={'reset'} callBack={resetResultCallback} disabled={resetDisabled}/>
+    const setSettingsToStorage = () => {
+        localStorage.setItem('startValue', JSON.stringify(valueSettings.startValue))
+        localStorage.setItem('maxValue', JSON.stringify(valueSettings.maxValue))
+        setIncrement(valueSettings.startValue)
+    }
+
+    const incrementCounter = () => {
+        setIncrement(increment + 1)
+    }
+
+    const resetCounter = () => {
+        setIncrement(valueSettings.startValue)
+    }
+
+    const startValue = (startValue: number) => {
+        setValueSettings({...valueSettings, startValue: startValue})
+    }
+    const maxValue = (maxValue: number) => {
+        setValueSettings({...valueSettings, maxValue: maxValue})
+    }
+
+    return (
+        <div className='App'>
+            <Settings
+                key={'startValue' || 'maxValue'}
+                incrementCounter={increment}
+                maxValue={valueSettings.maxValue}
+                startValue={valueSettings.startValue}
+                startSettingsValue={startValue}
+                maxSettingsValue={maxValue}
+                error={error}
+                setSettingsToStorage={setSettingsToStorage}/>
+            <div>
+                <Display
+                    resetCounter={resetCounter}
+                    incrementCounter={incrementCounter}
+                    increment={increment}
+                    maxValue={valueSettings.maxValue}
+                    startValue={valueSettings.startValue}
+                    error={error}/>
+            </div>
+
         </div>
-      </div>
-  );
+    );
 }
 
 export default App;
+
+
+// className={'AppButtonBlock'}
